30f25e6f0e12f1170a4444afb158ce36d2764daf
RomanTkachenko1996 <110996538+RomanTkachenko1996@users.noreply.github.com>
2023-02-07 21:15:58 +0200

    Task 3 resolved

--- src/Task3.java
+++ src/Task3.java
@@ -1,3 +1,7 @@
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Є масив:
  *
@@ -8,4 +12,14 @@
  * "0, 1, 2, 4, 5"
  */
 public class Task3 {
+    public static void main(String[] args) {
+       String[] array = new String[]{"1, 2, 0", "4, 5"};
+        String result = Arrays.stream(array)
+                .flatMap(s -> Arrays.stream(s.split(", ")))
+                .map(Integer::parseInt)
+                .sorted()
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
+        System.out.println(result);
+    }
 }
