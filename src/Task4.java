441d3f120c08f6f919fd8306aa197d230d48de0b
RomanTkachenko1996 <110996538+RomanTkachenko1996@users.noreply.github.com>
2023-02-08 17:01:02 +0200

    Task 3 resolved

--- src/Task4.java
+++ src/Task4.java
@@ -1,8 +1,10 @@
+import java.util.stream.Stream;
+
 /**
  * Використовуючи Stream.iterate, створіть безкінечний стрім випадкових чисел, але не використовуючи Math.random().
  *
  * Реалізуйте свій лінійний конгруентний генератор. Для цього почніть з x[0] = seed,
- * і далі кожний наступний елемент рахуйте за формулою на зразок x[n + 1] = 1 (a x[n] + c) % m для коректних значень a, c, та m.
+ * і далі кожний наступний елемент рахуйте за формулою на зразок x[n + 1] = 1 (a*x[n] + c) % m для коректних значень a, c, та m.
  *
  * Необхідно імплементувати метод, що приймає на вхід параметри a, c, та m, і повертає Stream<Long>.
  *
@@ -13,4 +15,24 @@
  *     m = 2^48 (2в степені48`)
  */
 public class Task4 {
+    /**
+     * x[n + 1] = 1 (a*x[n] + c) % m
+     */
+    public static Stream<Long> generateNumber (long seed, long a, long c, long m){
+        return Stream.iterate(seed, n->(a*(n+1)+c)%m)
+                .mapToLong(aLong -> aLong)
+                .boxed();
+    }
+}
+
+class TestGenerator{
+    public static void main(String[] args) {
+        long seed = System.currentTimeMillis();
+        long a = 25214903917L;
+        long c = 11L;
+        long m = (long) Math.pow(2,48);
+        Task4.generateNumber(seed, a, c, m)
+                .limit(10)
+                .forEach(System.out::println);
+    }
 }
