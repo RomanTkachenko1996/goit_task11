1c3eabc20a37bfc632a11bf656c71f5a8a27720d
RomanTkachenko1996 <110996538+RomanTkachenko1996@users.noreply.github.com>
2023-02-09 13:50:03 +0200

    Task 5 edited

--- src/Task5.java
+++ src/Task5.java
@@ -7,14 +7,20 @@ import java.util.stream.Stream;
  * коли у одного зі стрімів закінчаться елементи.
  */
 public class Task5 {
-    private static Stream<String> zip(Stream<String> stream1, Stream<String> stream2) {
-        List<String> strings = new ArrayList<>(Stream.concat(stream1, stream2).toList());
-        Collections.shuffle(strings);
-        return strings.stream();
+    private static <T> Stream<T> zip(Stream<T> stream1, Stream<T> stream2) {
+        List<T> list = new ArrayList<>();
+        Iterator<T> iterator1 = stream1.iterator();
+        Iterator<T> iterator2 = stream2.iterator();
+        while(iterator1.hasNext() && iterator2.hasNext()){
+            list.add(iterator1.next());
+            list.add(iterator2.next());
+        }
+        return list.stream().peek(System.out::println);
     }
     public static void main(String[] args) {
         Stream<String> stream1 = Stream.of("a", "c", "e","g");
         Stream<String> stream2 = Stream.of("b", "d", "f");
-        System.out.println("zip(stream1,stream2).toList() = " + zip(stream1, stream2).toList());
+        List<String> listOfString = zip(stream1, stream2).toList();
+
     }
 }
